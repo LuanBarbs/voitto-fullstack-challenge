826c2d7f029ed66b4c5aaa6f6952682025180b75
LuanBarbs <luan.barbosa@estudante.ufjf.br>
2025-02-15 18:31:33 -0300

    adicionando fetch para buscar e exibir mensagem básica vindo do backend

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // Página principal.
 export default function Home() {
-    return <></>;
+
+  const [message, setMessage] = useState("Loading!");
+
+  useEffect(function fetchData() {
+    fetch("http://localhost:8080/").then(
+      response => response.json()
+    ).then(
+      data => {
+        setMessage(data.message);
+      }
+    );
+  }, []);
+
+  return (
+    <div>{message}</div>
+  
+  );
 };
