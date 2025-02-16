b5153482bc63a2e44bf5cf98da3ba47dc5c9dad6
LuanBarbs <luan.barbosa@estudante.ufjf.br>
2025-02-16 19:42:08 -0300

    criando a rota e o controlador para matriculas

---
+++ server/src/controllers/enrollmentsController.js
@@ -0,0 +1,32 @@
+import db from "../database.js"; // Importa o banco de dados.
+
+// Função que insere uma nova matrícula no banco.
+export async function insertEnrollment(req, res) {
+    const enrollment = req.body;
+
+    if(!enrollment.course_id || !enrollment.studant_name) {
+        return res.status(400).json({
+            error: "ID do curso e nome do aluno são obrigatórios."
+        });
+    }
+
+    db.run("INSERT INTO enrollments (course_id, studant_name) VALUES (?, ?)",
+        [enrollment.course_id, enrollment.studant_name],
+        (error) => {
+            if (error) return res.status(500).json({ error: error.message });
+
+            res.status(201).json({ message: "Curso inserido com sucesso!" });
+        }
+    );
+};
+
+// Função que lista alunos matriculados em um curso. 
+export async function getEnrollmentsByCourse(req, res) {
+    const { courseId } = req.params;
+
+    db.all("SELECT * FROM enrollments WHERE course_id = ?", [courseId], (error, rows) => {
+        if (error) return res.status(500).json({ error: error.message });
+
+        res.json(rows);
+    });
+};
--- server/src/routes/enrrolmentsRoutes.js
+++ server/src/routes/enrrolmentsRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 
+import { insertEnrollment, getEnrollmentsByCourse } from "../controllers/enrollmentsController.js";
+
 const enrollmentsRouter = express.Router();
 
 // Criando as rotas para as operações de matrículas.
-enrollmentsRouter.post("/", () => {});
-enrollmentsRouter.get("/:courseId", () => {});
+enrollmentsRouter.post("/", insertEnrollment);
+enrollmentsRouter.get("/:courseId", getEnrollmentsByCourse);
 
 export default enrollmentsRouter;
